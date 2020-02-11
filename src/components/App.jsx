c51a6e3077ebfa2b5a5b0cd2a884ac1ba3b54a3a
Student <example@example.com>
2020-02-11 15:21:13 -0800

    use material design.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Header from './Header';
 import TicketList from './LocationList';
-
+import NavBar from './NavBar'
 
 
 function App(){
     return (
         <div>
             <Header/>
+            <NavBar />
             <TicketList/>
         </div>
     );
